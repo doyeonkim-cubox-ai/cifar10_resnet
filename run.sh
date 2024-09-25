fc9582f872dc6835a1dfefd91af3647c33cd2a16
doyeonkim-cubox-ai <doyeon.kim@cubox.ai>
2024-09-25 17:57:49 +0900

    TEST : add loop
    - add loop for convenience

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=cifar10_resnet
-#SBATCH --output="./logs/cifar10_resnet"
-#SBATCH --nodelist=nv178
+#SBATCH --output="./logs/resnets"
+#SBATCH --nodelist=nv174
 #SBATCH --gpus=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
@@ -21,23 +21,34 @@ date
 
 # ex) srun python -m mnist_resnet50.train
 
-srun python -m cifar10_resnet.train -model resnet20
-srun python -m cifar10_resnet.test -model resnet20
-srun python -m cifar10_resnet.train -model resnet32
-srun python -m cifar10_resnet.test -model resnet32
-srun python -m cifar10_resnet.train -model plain20
-srun python -m cifar10_resnet.test -model plain20
-srun python -m cifar10_resnet.train -model plain32
-srun python -m cifar10_resnet.test -model plain32
-srun python -m cifar10_resnet.train -model resnet44
-srun python -m cifar10_resnet.test -model resnet44
-srun python -m cifar10_resnet.train -model plain44
-srun python -m cifar10_resnet.test -model plain44
-srun python -m cifar10_resnet.train -model resnet56
-srun python -m cifar10_resnet.test -model resnet56
-srun python -m cifar10_resnet.train -model plain56
-srun python -m cifar10_resnet.test -model plain56
-srun python -m cifar10_resnet.train -model resnet110
-srun python -m cifar10_resnet.test -model resnet110
-srun python -m cifar10_resnet.train -model resnet1202
-srun python -m cifar10_resnet.test -model resnet1202
+#srun python -m cifar10_resnet.train -model resnet20
+#srun python -m cifar10_resnet.test -model resnet20
+#srun python -m cifar10_resnet.train -model resnet32
+#srun python -m cifar10_resnet.test -model resnet32
+#srun python -m cifar10_resnet.train -model plain20
+#srun python -m cifar10_resnet.test -model plain20
+#srun python -m cifar10_resnet.train -model plain32
+#srun python -m cifar10_resnet.test -model plain32
+#srun python -m cifar10_resnet.train -model resnet44
+#srun python -m cifar10_resnet.test -model resnet44
+#srun python -m cifar10_resnet.train -model plain44
+#srun python -m cifar10_resnet.test -model plain44
+#srun python -m cifar10_resnet.train -model resnet56
+#srun python -m cifar10_resnet.test -model resnet56
+#srun python -m cifar10_resnet.train -model plain56
+#srun python -m cifar10_resnet.test -model plain56
+#srun python -m cifar10_resnet.train -model resnet110
+#srun python -m cifar10_resnet.test -model resnet110
+#srun python -m cifar10_resnet.train -model resnet1202
+#srun python -m cifar10_resnet.test -model resnet1202
+
+#cnt=1
+#while [ 1 == 1 ]
+#do
+#  if [ $cnt -eq 5 ]; then
+#    break
+#  fi
+#  srun python -m cifar10_resnet.train -model resnet56
+#  srun python -m cifar10_resnet.test -model resnet56
+#  let cnt++
+#done
